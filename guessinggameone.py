dffe7ab7e62f6808c414e461a73e52cf99478c6b
Diego <136518321+tenochca@users.noreply.github.com>
2023-08-07 21:57:34 -0500

    added problem description

--- guessinggameone.py
+++ guessinggameone.py
@@ -1,4 +1,5 @@
-#guessinggameone
+#Generate a random number between 1 and 9 (including 1 and 9). 
+#Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
 
 import random 
 

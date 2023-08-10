c61659713ef285e19bb86f08e82ba1995ccfd53d
Diego <136518321+tenochca@users.noreply.github.com>
2023-08-09 22:52:48 -0500

    Update primedivisorfunction.py
    wrong file upload, pasted full code instead

--- primedivisorfunction.py
+++ primedivisorfunction.py
@@ -1,7 +1,15 @@
 #Ask the user for a number and determine whether the number is prime or not. 
 
+divisors = [] #init empty list to hold divsors
 def prime_num(x):
-    num_list = str(range(0,x+1))
-    print(num_list)
+    num_list = list(range(1,x+1)) #list of numbers leading up to x
+    for i in num_list: #iterating through the list
+        if x%i == 0: #if the number is a divisor of x
+            divisors.append(i) #add to the list
+    if len(divisors) == 2: #outside the loop, if the len of the list = 2
+        print('Prime') #the number is prime
+    else:
+        print('Composite') #otherwise it is composite
 
-prime_num(10)
+num = eval(input('Enter a number: '))
+prime_num(num)

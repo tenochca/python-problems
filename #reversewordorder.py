2e12fa9ff32884a410590b8ffdc55602672ae51d
Diego <136518321+tenochca@users.noreply.github.com>
2023-08-19 15:28:39 -0500

    Add files via upload

---
+++ #reversewordorder.py
@@ -0,0 +1,18 @@
+#Write a program (using functions!) that asks the user for a long string containing multiple words. 
+#Print back to the user the same string, except with the words in backwards order.
+
+def reverse(s):
+    'takes a string a reverses the word order'
+    if len(s) == 1:
+        return s[0]
+    else:
+        firstitem = s[0]
+        s.remove(firstitem)
+        x = reverse(s) + ' ' + firstitem
+        return x
+    
+
+string = (input('Enter a sentence: ')).split()
+print(reverse(string))
+
+

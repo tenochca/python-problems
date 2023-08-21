b9da7cae89defe56932af969a15ef3b5c4d8e9b1
Diego <136518321+tenochca@users.noreply.github.com>
2023-08-20 22:25:32 -0500

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,21 @@
+""" Write a password generator in Python. Be creative with how you generate passwords - 
+strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
+The passwords should be random, generating a new password every time the user asks for a new 
+password. Include your run-time code in a main method. """
+
+import random as rd
+
+import string
+
+pool = string.ascii_letters + '0123456789' + "!#$%&'()*+-/:;<=>?@[\]^_~"
+
+
+def password(k: int):
+    'creates a password based on k number of characters'
+    passwrd = ''.join(rd.choices(pool, k=k))
+    return passwrd
+
+print(password(10))
+
+print(string.punctuation)
+print(str(range(0,9)))
